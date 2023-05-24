1e49f3b316a886995c87b83a723c3e529bf1da54
Exetizu <exetizu@wp.pl>
2023-05-24 18:31:57 +0200

    test

--- public/script.js
+++ public/script.js
@@ -1,32 +1,34 @@
+var socket = io();
+socket.on("connect", () => {
+  socket.emit("new-client-append", { code: "" });
+  socket.on("new-client-append", (code) => {
+    console.log("abc");
+    editor.getSession().setValue(code.code);
+    document.getElementsByTagName("iframe")[0].srcdoc = editor
+      .getSession()
+      .getValue();
+  });
+  console.log("User connected");
+  document.getElementById("editor").addEventListener("keyup", () => {
+    socket.emit("codeSend", {
+      code: editor.getSession().getValue(),
+    });
+    document.getElementsByTagName("iframe")[0].srcdoc = editor
+      .getSession()
+      .getValue();
+  });
+});
+socket.on("disconnect", () => {
+  console.log("User disconnected");
+});
+socket.on("codeReceive", (code) => {
+  editor.getSession().setValue(code.code);
+  document.getElementsByTagName("iframe")[0].srcdoc = editor
+    .getSession()
+    .getValue();
+});
 
-        var socket = io();
-        socket.on('connect',()=>{
-            socket.emit('new-client-append', {code: ""})
-            socket.on("new-client-append",(code)=>{
-                console.log("abc");
-                editor.getSession().setValue(code.code)
-                document.getElementsByTagName("iframe")[0].srcdoc = editor.getSession().getValue()
-            })
-            console.log("User connected");
-            document.getElementById("editor").addEventListener("keyup",()=>{
-                socket.emit('codeSend',{
-                    code: editor.getSession().getValue()
-                })
-                document.getElementsByTagName("iframe")[0].srcdoc = editor.getSession().getValue()
-            })
-        })
-        socket.on('disconnect',()=>{
-            console.log("User disconnected");
-        });
-        socket.on("codeReceive",(code)=>{
-            editor.getSession().setValue(code.code)
-            document.getElementsByTagName("iframe")[0].srcdoc = editor.getSession().getValue()
-
-        })
-
-
-        //ace config /////////////////////////////////////////////////////////////
-        var editor = ace.edit("editor");
-        editor.setTheme("ace/theme/tomorrow_night");
-        editor.session.setMode("ace/mode/html");
-        
+//ace config /////////////////////////////////////////////////////////////
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/tomorrow_night");
+editor.session.setMode("ace/mode/html");
